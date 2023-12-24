a85b356f8b0863655b4f1713b5a7e059fe629100
Abul-Farhad <muhammadsakib1516@gmail.com>
2023-12-24 09:53:12 +0600

    Added Django by mistake. Now removing it.

--- DjangoAPI/EmployeeApp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import re_path,path
-from EmployeeApp import views
-
-urlpatterns = [
-    # path('department/', views.departmentApi)
-    re_path(r'^department$',views.departmentApi),
-    re_path(r'^department/([0-9]+)$',views.departmentApi)
-]
--- DjangoAPI/GamingClientApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GamingclientappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'GamingClientApp'
